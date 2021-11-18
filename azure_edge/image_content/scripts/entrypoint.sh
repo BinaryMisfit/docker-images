180cb9c3aeed42bdb00344abe4ac3094bcf15694
Willie Roberts <diagoza@me.com>
2021-11-18 13:08:22 +0200

    Added sudo to azure_edge

--- azure_edge/image_content/scripts/entrypoint.sh
+++ azure_edge/image_content/scripts/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 printf "Running SQL Server"
 if [[ -d /sqldata ]]; then
-  chown -R mssql: /sqldata
+  sudo chown -R mssql: /sqldata
 fi
 
 if [[ -d /var/opt/mssql/data ]]; then
-  chown -R mssql: /var/opt/mssql/data
+  sudo chown -R mssql: /var/opt/mssql/data
 fi
 
 exec /opt/mssql/bin/sqlservr
